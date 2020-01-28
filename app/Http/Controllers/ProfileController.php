362aea1ef60770f020aec1c4c8b62d6452d91d4a
reenahukum <big.b.it.traininig@gmail.com>
2020-01-29 01:18:13 +0545

    Profile crud with no validation

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,10 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Profile;
 
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function index()
+    {
+        $profiles = Profile::latest()->paginate(5);
+
+        return view('profiles.index', compact('profiles'))
+        ->with('i', (request() -> input('page', 1) -1) * 5);
+    }
+
+    public function create()
+    {
+        return view('profiles.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        Profile::create($request->all());
+
+        return redirect()->route('profiles.index')
+                        ->with('success', 'Profiles created successfully');
+    }
 }
